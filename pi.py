33c2f8ffe318d1f2b75c3be936c8edd5d0658967
Rohit-Shastry <76892992+Rohit-Shastry@users.noreply.github.com>
2022-01-03 20:54:53 +0530

    Add files via upload

---
+++ pi.py
@@ -0,0 +1,16 @@
+import time
+
+num = input("Please enter the number of digits:- ")
+print("[+]Printing pi in 3,2,1..")
+time.sleep(3)
+
+try:
+    # import version included with old SymPy
+    from sympy.mpmath import mp
+
+except ImportError:
+    # import newer version
+    from mpmath import mp
+
+mp.dps = num  # set number of digits
+print(mp.pi)   # print pi to a thousand places
